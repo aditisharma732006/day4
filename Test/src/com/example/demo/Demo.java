5b6b603a25c437053758602369b499eaa0df02f1
Aditi Sharma <aditisharma732006@gmail.com>
2026-06-30 23:28:36 +0530

    basic string application - manual DI and added .jar files manually from mvn repository

---
+++ Test/src/com/example/demo/Demo.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Demo {
+
+	public static void main(String [] args) {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Employee e=(Employee) ac.getBean("emp");
+		
+		System.out.println("Employee id is " + e.getEmpid());
+		System.out.println("Employee name is " + e.getName());
+		System.out.println("Employee salary is " + e.getSalary());
+		
+	} 
+}
---
+++ Test/src/com/example/demo/Employee.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+public class Employee {
+	private int empid;
+	private String name;
+	private String salary;
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	
+}
