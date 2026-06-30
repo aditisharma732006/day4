97bb67037639b7e6e2c390ca84c3197106c49b03
Aditi Sharma <aditisharma732006@gmail.com>
2026-06-30 23:32:48 +0530

    productClient is making sync call to this service

---
+++ ProductService/src/main/java/com/example/demo/ProductController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+	@GetMapping("/product")
+	public String getProduct() {
+		return "laptop price= -837573982";
+	}
+}
